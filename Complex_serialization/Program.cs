a61b2af683a24107900b81aa3257f8dfbd48da09
Aydos Zhengissov <aydoszhengissov@gmail.com>
2017-04-17 03:26:56 +0600

    a

---
+++ Complex_serialization/Program.cs
@@ -0,0 +1,65 @@
+﻿using Newtonsoft.Json;
+using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml.Serialization;
+
+namespace Complex_serialization
+{
+    public class Complex
+    {
+        public double A;
+        public double B;
+        public static Complex operator +(Complex l, Complex r)
+        {
+            Complex res = new Complex();
+            res.A = l.A + r.A;
+            res.B = l.B + r.B;
+            return res;
+        }
+        public override string ToString()
+        {
+            return string.Format("{0} + {1}i", A, B);
+        }
+    }
+
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            F2();
+        }
+
+        private static void F2()
+        {
+            Complex c = new Complex();
+            c.A = 6;
+            c.B = 8;
+
+            JsonSerializer js = new JsonSerializer();
+            string res = JsonConvert.SerializeObject(c);
+
+            FileStream fs = new FileStream("complex.json", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            StreamWriter sw = new StreamWriter(fs);
+            sw.WriteLine(res);
+
+            sw.Close();
+            fs.Close();
+
+        }
+
+        private static void F1()
+        {
+            FileStream fs = new FileStream("complex.xml", FileMode.OpenOrCreate, FileAccess.ReadWrite);
+            Complex c = new Complex();
+            c.A = 5;
+            c.B = 7;
+            XmlSerializer xs = new XmlSerializer(typeof(Complex));
+            xs.Serialize(fs, c);
+            fs.Close();
+        }
+    }
+}
---
+++ FarManager/Program.cs
@@ -0,0 +1,80 @@
+﻿using System;
+using System.IO;
+
+namespace FarManager
+{
+    class MainClass
+    {
+        static void ShowInfo(DirectoryInfo directory, int cursor)
+        {
+            Console.BackgroundColor = ConsoleColor.Black;
+            int index = 0;
+            foreach (FileSystemInfo fi in directory.GetFileSystemInfos())
+            {
+                if (index == cursor)
+                    Console.ForegroundColor = ConsoleColor.Blue;
+                else
+                    Console.ForegroundColor = ConsoleColor.White;
+                index++;
+                Console.WriteLine(fi.Name);
+            }
+        }
+
+        static void Main(string[] args)
+        {
+            int cursor = 0;
+            DirectoryInfo directory = new DirectoryInfo(@"C:\Users\admin12\Desktop");
+            while (true)
+            {
+                Console.Clear();
+                ShowInfo(directory, cursor);
+                ConsoleKeyInfo pressedKey = Console.ReadKey();
+                if (pressedKey.Key == ConsoleKey.UpArrow)
+                {
+                    if (cursor > 0)
+                    {
+                        cursor--;
+                    }
+                }
+                if (pressedKey.Key == ConsoleKey.DownArrow)
+                {
+                    if (cursor < directory.GetFileSystemInfos().Length - 1)
+                    {
+                        cursor++;
+                    }
+                }
+                if (pressedKey.Key == ConsoleKey.Enter)
+                {
+                    FileSystemInfo fi = directory.GetFileSystemInfos()[cursor];
+                    if (fi.GetType() == typeof(DirectoryInfo))
+                    {
+                        directory = new DirectoryInfo(fi.FullName);
+                        cursor = 0;
+                    }
+
+                    else
+                    {
+                        string ext = Path.GetExtension(fi.FullName);
+                        if (ext == ".txt")
+                        {
+                            Console.Clear();
+                            StreamReader sr = new StreamReader(fi.FullName);
+                            Console.WriteLine(sr.ReadToEnd());
+                            sr.Close();
+                            while (Console.ReadKey(true).Key != ConsoleKey.Escape)
+                            {
+                                // do nothing until escape
+                            }
+                        }
+                    }
+                }
+                if (pressedKey.Key == ConsoleKey.Escape)
+                {
+                    directory = Directory.GetParent(directory.FullName);
+                }
+                if (pressedKey.Key == ConsoleKey.Backspace)
+                    break;
+            }
+        }
+    }
+}
