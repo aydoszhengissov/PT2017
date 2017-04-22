55dc9f9233f8352dd6acb128983808bc07989a68
Aydos Zhengissov <aydoszhengissov@gmail.com>
2017-04-22 19:59:16 +0600

    PhoneBook added

---
+++ PhoneBook2/Form1.Designer.cs
@@ -0,0 +1,196 @@
+﻿namespace PhoneBook2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            System.ComponentModel.ComponentResourceManager resources = new System.ComponentModel.ComponentResourceManager(typeof(Form1));
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle5 = new System.Windows.Forms.DataGridViewCellStyle();
+            System.Windows.Forms.DataGridViewCellStyle dataGridViewCellStyle6 = new System.Windows.Forms.DataGridViewCellStyle();
+            this.button1 = new System.Windows.Forms.Button();
+            this.txtSearch = new System.Windows.Forms.TextBox();
+            this.button2 = new System.Windows.Forms.Button();
+            this.dataGridView1 = new System.Windows.Forms.DataGridView();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.Transparent;
+            this.button1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button1.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button1.Image = ((System.Drawing.Image)(resources.GetObject("button1.Image")));
+            this.button1.Location = new System.Drawing.Point(662, 14);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(30, 30);
+            this.button1.TabIndex = 4;
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            this.button1.Paint += new System.Windows.Forms.PaintEventHandler(this.button1_Paint_1);
+            // 
+            // txtSearch
+            // 
+            this.txtSearch.Location = new System.Drawing.Point(12, 14);
+            this.txtSearch.Multiline = true;
+            this.txtSearch.Name = "txtSearch";
+            this.txtSearch.Size = new System.Drawing.Size(396, 30);
+            this.txtSearch.TabIndex = 6;
+            this.txtSearch.TextChanged += new System.EventHandler(this.txtSearch_TextChanged);
+            // 
+            // button2
+            // 
+            this.button2.BackColor = System.Drawing.Color.Transparent;
+            this.button2.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button2.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button2.Image = ((System.Drawing.Image)(resources.GetObject("button2.Image")));
+            this.button2.Location = new System.Drawing.Point(626, 14);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(30, 30);
+            this.button2.TabIndex = 7;
+            this.button2.UseVisualStyleBackColor = false;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            this.button2.Paint += new System.Windows.Forms.PaintEventHandler(this.button2_Paint_1);
+            // 
+            // dataGridView1
+            // 
+            dataGridViewCellStyle5.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(224)))), ((int)(((byte)(238)))), ((int)(((byte)(241)))));
+            this.dataGridView1.AlternatingRowsDefaultCellStyle = dataGridViewCellStyle5;
+            this.dataGridView1.AutoSizeColumnsMode = System.Windows.Forms.DataGridViewAutoSizeColumnsMode.Fill;
+            this.dataGridView1.AutoSizeRowsMode = System.Windows.Forms.DataGridViewAutoSizeRowsMode.DisplayedCellsExceptHeaders;
+            this.dataGridView1.BackgroundColor = System.Drawing.Color.FromArgb(((int)(((byte)(245)))), ((int)(((byte)(243)))), ((int)(((byte)(245)))));
+            this.dataGridView1.BorderStyle = System.Windows.Forms.BorderStyle.None;
+            dataGridViewCellStyle6.Alignment = System.Windows.Forms.DataGridViewContentAlignment.MiddleLeft;
+            dataGridViewCellStyle6.BackColor = System.Drawing.SystemColors.Control;
+            dataGridViewCellStyle6.Font = new System.Drawing.Font("Microsoft Sans Serif", 8.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            dataGridViewCellStyle6.ForeColor = System.Drawing.Color.White;
+            dataGridViewCellStyle6.SelectionBackColor = System.Drawing.Color.FromArgb(((int)(((byte)(34)))), ((int)(((byte)(64)))), ((int)(((byte)(79)))));
+            dataGridViewCellStyle6.SelectionForeColor = System.Drawing.SystemColors.HighlightText;
+            dataGridViewCellStyle6.WrapMode = System.Windows.Forms.DataGridViewTriState.True;
+            this.dataGridView1.ColumnHeadersDefaultCellStyle = dataGridViewCellStyle6;
+            this.dataGridView1.ColumnHeadersHeight = 25;
+            this.dataGridView1.ColumnHeadersHeightSizeMode = System.Windows.Forms.DataGridViewColumnHeadersHeightSizeMode.DisableResizing;
+            this.dataGridView1.Location = new System.Drawing.Point(11, 50);
+            this.dataGridView1.Name = "dataGridView1";
+            this.dataGridView1.RowHeadersVisible = false;
+            this.dataGridView1.Size = new System.Drawing.Size(717, 377);
+            this.dataGridView1.TabIndex = 8;
+            this.dataGridView1.Paint += new System.Windows.Forms.PaintEventHandler(this.dataGridView1_Paint_1);
+            // 
+            // button3
+            // 
+            this.button3.BackColor = System.Drawing.Color.Transparent;
+            this.button3.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button3.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button3.Image = ((System.Drawing.Image)(resources.GetObject("button3.Image")));
+            this.button3.Location = new System.Drawing.Point(590, 14);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(30, 30);
+            this.button3.TabIndex = 9;
+            this.button3.UseVisualStyleBackColor = false;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            this.button3.Paint += new System.Windows.Forms.PaintEventHandler(this.button3_Paint);
+            // 
+            // button4
+            // 
+            this.button4.BackColor = System.Drawing.Color.Transparent;
+            this.button4.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button4.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button4.Image = ((System.Drawing.Image)(resources.GetObject("button4.Image")));
+            this.button4.Location = new System.Drawing.Point(554, 14);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(30, 30);
+            this.button4.TabIndex = 10;
+            this.button4.UseVisualStyleBackColor = false;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            this.button4.Paint += new System.Windows.Forms.PaintEventHandler(this.button4_Paint);
+            // 
+            // button5
+            // 
+            this.button5.BackColor = System.Drawing.Color.Transparent;
+            this.button5.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button5.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button5.Image = ((System.Drawing.Image)(resources.GetObject("button5.Image")));
+            this.button5.Location = new System.Drawing.Point(518, 14);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(30, 30);
+            this.button5.TabIndex = 11;
+            this.button5.UseVisualStyleBackColor = false;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            this.button5.Paint += new System.Windows.Forms.PaintEventHandler(this.button5_Paint);
+            // 
+            // button6
+            // 
+            this.button6.BackColor = System.Drawing.Color.Transparent;
+            this.button6.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.button6.ForeColor = System.Drawing.SystemColors.ControlText;
+            this.button6.Image = ((System.Drawing.Image)(resources.GetObject("button6.Image")));
+            this.button6.Location = new System.Drawing.Point(698, 14);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(30, 30);
+            this.button6.TabIndex = 12;
+            this.button6.UseVisualStyleBackColor = false;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            this.button6.Paint += new System.Windows.Forms.PaintEventHandler(this.button6_Paint);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(243)))), ((int)(((byte)(245)))), ((int)(((byte)(243)))));
+            this.ClientSize = new System.Drawing.Size(740, 431);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.dataGridView1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.txtSearch);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.dataGridView1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox txtSearch;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.DataGridView dataGridView1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+    }
+}
+
---
+++ PhoneBook2/Form1.cs
@@ -0,0 +1,223 @@
+﻿using MySql.Data.MySqlClient;
+using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace PhoneBook2
+{
+    public partial class Form1 : Form
+    {
+        MySqlConnection mySqlConnection;
+        MySqlDataAdapter mySqlDataAdapter = new MySqlDataAdapter();
+        MySqlCommandBuilder mySqlCommandBuilder;
+        DataSet ds = new DataSet();
+        BindingSource bindingSource;
+        int pgSize = 15;
+        int curPage;
+        bool sort = false;
+        bool ascDate = false;
+
+        public Form1()
+        {
+            InitializeComponent();
+            mySqlConnection = new MySqlConnection(
+                         "SERVER=localhost;" +
+                         "DATABASE=pt2017;" +
+                         "UID=root;" +
+                         "PASSWORD=;");
+            curPage = 1;
+            ShowDB(curPage);
+        }
+
+        private void ShowDB(int curPage)
+        {
+            ds.Tables.Clear();
+            mySqlConnection.Open();
+            int PreviousPageOffSet = (curPage - 1) * pgSize;
+            string query;
+            if (!sort)
+            {
+                query = "SELECT * FROM phonebook";
+            }
+            else
+            {
+                if (ascDate)
+                {
+                    query = "SELECT * FROM phonebook ORDER BY date ASC";
+                }
+                else
+                {
+                    query = "SELECT * FROM phonebook ORDER BY date DESC";
+                }
+            }
+            mySqlDataAdapter = new MySqlDataAdapter(query, mySqlConnection);
+            mySqlCommandBuilder = new MySqlCommandBuilder(mySqlDataAdapter);
+            mySqlDataAdapter.UpdateCommand = mySqlCommandBuilder.GetUpdateCommand();
+            mySqlDataAdapter.DeleteCommand = mySqlCommandBuilder.GetDeleteCommand();
+            mySqlDataAdapter.InsertCommand = mySqlCommandBuilder.GetInsertCommand();
+            ds = new DataSet();
+            mySqlDataAdapter.Fill(ds, "allrows");
+            mySqlDataAdapter.Fill(ds, PreviousPageOffSet, pgSize, "phonebook");
+            bindingSource = new BindingSource();
+            bindingSource.DataSource = ds;
+            dataGridView1.DataSource = bindingSource;
+            dataGridView1.DataMember = "phonebook";
+            mySqlConnection.Close();
+        }
+        
+        public void Search()
+        {
+            mySqlConnection.Open();
+            ds.Clear();
+            string query = "SELECT * FROM phonebook WHERE name like '%" + txtSearch.Text + "%'";
+            mySqlDataAdapter = new MySqlDataAdapter(query, mySqlConnection);
+            mySqlCommandBuilder = new MySqlCommandBuilder(mySqlDataAdapter);
+            mySqlDataAdapter.UpdateCommand = mySqlCommandBuilder.GetUpdateCommand();
+            mySqlDataAdapter.DeleteCommand = mySqlCommandBuilder.GetDeleteCommand();
+            mySqlDataAdapter.InsertCommand = mySqlCommandBuilder.GetInsertCommand();
+            ds = new DataSet();
+            mySqlDataAdapter.Fill(ds, "search");
+            bindingSource = new BindingSource();
+            bindingSource.DataSource = ds;
+            dataGridView1.DataSource = bindingSource;
+            dataGridView1.DataMember = "search";
+            mySqlConnection.Close();
+        }
+
+        public void Delete()
+        {
+            mySqlConnection.Open();
+            int rowIndex = dataGridView1.CurrentCell.RowIndex;
+            string deleteRow = dataGridView1.Rows[rowIndex].Cells[2].Value.ToString();
+
+            MySqlCommand cmd = new MySqlCommand(@"
+                DELETE FROM phonebook WHERE (phonenumber = '" + deleteRow + "')", mySqlConnection);
+            cmd.ExecuteNonQuery();
+            mySqlConnection.Close();
+            ShowDB(curPage);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            mySqlDataAdapter.Update(ds.Tables[1]);
+            //ShowDB((ds.Tables["allrows"].Rows.Count) / pgSize + 1);
+        }
+
+        private void txtSearch_TextChanged(object sender, EventArgs e)
+        {
+            Search();
+            if (txtSearch.TextLength == 0)
+            {
+                ShowDB(curPage);
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Delete();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            curPage++;
+            if (curPage >= ((ds.Tables["allrows"].Rows.Count) / pgSize + 1))
+            {
+                curPage = ((ds.Tables["allrows"].Rows.Count) / pgSize + 1);
+            }
+            ShowDB(curPage);
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            curPage--;
+            if (curPage <= 0)
+            {
+                curPage = 1;
+            }
+            ShowDB(curPage);
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            if (!sort)
+            {
+                sort = true;
+            }
+            if (ascDate)
+                ascDate = false;
+            else
+                ascDate = true;
+            ShowDB(curPage);
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            curPage = 1;
+            sort = false;
+            ascDate = false;
+            ShowDB(curPage);
+        }
+
+        private void dataGridView1_Paint_1(object sender, PaintEventArgs e)
+        {
+            dataGridView1.ColumnHeadersDefaultCellStyle.BackColor = Color.FromArgb(0, 176, 221);
+            dataGridView1.EnableHeadersVisualStyles = false;
+            dataGridView1.Rows[0].ReadOnly = true;
+            DataGridViewColumn column = dataGridView1.Columns[0];
+            column.Width = 30;
+            DataGridViewColumn column1 = dataGridView1.Columns[1];
+            column1.Width = 125;
+            DataGridViewColumn column2 = dataGridView1.Columns[2];
+            column2.Width = 100;
+            DataGridViewColumn column3 = dataGridView1.Columns[3];
+            column3.Width = 100;
+        }
+
+        private void button1_Paint(object sender, PaintEventArgs e)
+        {
+            button1.FlatAppearance.BorderSize = 0;
+        }
+
+        private void button2_Paint(object sender, PaintEventArgs e)
+        {
+            button2.FlatAppearance.BorderSize = 0;
+        }
+
+
+        private void button4_Paint(object sender, PaintEventArgs e)
+        {
+            button4.FlatAppearance.BorderSize = 0;
+        }
+
+        private void button3_Paint(object sender, PaintEventArgs e)
+        {
+            button3.FlatAppearance.BorderSize = 0;
+        }
+
+        private void button2_Paint_1(object sender, PaintEventArgs e)
+        {
+            button2.FlatAppearance.BorderSize = 0;
+        }
+
+        private void button1_Paint_1(object sender, PaintEventArgs e)
+        {
+            button1.FlatAppearance.BorderSize = 0;
+        }
+
+        private void button5_Paint(object sender, PaintEventArgs e)
+        {
+            button5.FlatAppearance.BorderSize = 0;
+        }
+
+        private void button6_Paint(object sender, PaintEventArgs e)
+        {
+            button6.FlatAppearance.BorderSize = 0;
+        }
+    }
+}
